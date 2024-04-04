8ec9cc6ea3050cbcd42a2989aaacba1d14c4a68e
Xenondq <almaz.eyupensar@gmail.com>
2024-04-04 14:25:14 +0100

    app.js completed

--- app.js
+++ app.js
@@ -1,9 +1,26 @@
-const express = require("express")
-const app = express()
-const port = process.env.PORT || 3001
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const httpStatusCode = require("http-status-codes");
+var cors = require("cors");
 
-app.get("/", (req ,res ) => {
-    res.json({
-        message: "HostPage"
-    })
-})
+var app = require('express')();
+const router = require('express').Router();
+
+const port = process.env.PORT || 5001;
+
+const socketConstants = require("./constants/socketConstants");
+
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+const userController = require("./controllers/userController");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+
+mongoose.connect("mongodb://localhost:27017/ChatApp", { useNewUrlParser: true, useUnifiedTopology: true });
+
+app.listen(port, () => console.log(port));
+
+app.use(userController.router());
